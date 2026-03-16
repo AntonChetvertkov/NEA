10ba133a0c72ca9ac4a4a56246d733d553887f09
Anton Chetvertkov <antonchetvertkov@gmail.com>
2026-03-16 18:23:56 +0000

    minor bug fixes

--- src/main.cpp
+++ src/main.cpp
@@ -13,37 +13,44 @@ void setup() {
     Serial.begin(115200);
     oled.init();
     oled.home();
-    oled.clear();
-    gpsSerial.begin(9600, SERIAL_8N1, GPS_RX, GPS_TX);
-    oled.println("NEO-7M GPS Test");
-    oled.println("---------------");
+    oled.setContrast(255);
+    // oled.clear();
+    // gpsSerial.begin(9600, SERIAL_8N1, GPS_RX, GPS_TX);
+    // oled.println("NEO-7M GPS Test");
+    // oled.println("---------------");
+    oled.println("ROWWELL-1-Starting up");
     oled.update();
-}
+    delay(10000);
+
+
+}   
 
 void loop() {
-    oled.clear();
-    oled.home();
-    while (gpsSerial.available()) {
-        gps.encode(gpsSerial.read());
-    }
+  oled.clear();
+  oled.home();
+    
 
-    static unsigned long last = 0;
-    if (millis() - last > 1000) {
-        last = millis();
+  while (gpsSerial.available()) {
+      gps.encode(gpsSerial.read());
+  }
 
-        oled.println("Satellites: ", gps.satellites.value());
-        oled.println("Fix valid: ", gps.location.isValid() ? "YES" : "NO");
+  static unsigned long last = 0;
+  if (millis() - last > 1000) {
+      last = millis();
 
-        if (gps.location.isValid()) {
-            oled.println("Lat: ", gps.location.lat());
-            oled.println("Lng: ", gps.location.lng());
-            oled.println("Alt: ", gps.altitude.meters());
-            oled.println("Speed: ", gps.speed.kmph());
-            oled.println("HDOP: ", gps.hdop.hdop());
-        } else {
-            oled.println("Waiting for fix...");
-        }
-        oled.println("---------------");
-        oled.update();
-    }
+      oled.println("Satellites: "+ String(gps.satellites.value()));
+      oled.println("Fix valid: "+ String(gps.location.isValid() ? "YES" : "NO"));
+
+      if (gps.location.isValid()) {
+          oled.println("Lat: "+ String(gps.location.lat()));
+          oled.println("Lng: "+ String(gps.location.lng()));
+          oled.println("Alt: "+ String(gps.altitude.meters()));
+          oled.println("Speed: "+ String(gps.speed.kmph()));
+          oled.println("HDOP: "+ String(gps.hdop.hdop()));
+      } else {
+          oled.println("Waiting for fix...");
+      }
+      oled.println("---------------");
+  }
+  oled.update();
 }
